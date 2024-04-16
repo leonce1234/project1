5369f97466abc207b1ed86ce0dc41faa5661fbf6
leonce1234 <163483728+leonce1234@users.noreply.github.com>
2024-04-16 19:56:39 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World My name is Davy Leonce!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
